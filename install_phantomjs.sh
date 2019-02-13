1df0d7dbb9076025a8140c609288d4c433512b0f
Jan Brodda <git@janbrodda.de>
2019-02-13 21:54:50 +0100

    .

--- install_phantomjs.sh
+++ install_phantomjs.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2 -O phantomjs.tar.bz2
+pwd
+ls
+wget -q https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2 -O phantomjs.tar.bz2
 mkdir phantomjs
 tar xjf phantomjs.tar.bz2 -C phantomjs --strip 1
+rm phantomjs.tar.bz2
